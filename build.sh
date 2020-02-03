6856784441d8b3fb09595173f8970d0e49a819b9
Martijn Vogten <martijn@vogten.nl>
2020-02-03 22:57:05 +0100

    more cleanup

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e -x
 
-# Builds the libmv8.so/dylib shared library for use with the mv8 Java classes.
-# It includes the precompiled V8 static library which is taken from the
-# Ruby libv8 gem by Charles Lowell.
-# Header files are included from the V8 gem and the Java JDK home directory
+# Builds both Java .jar and the libmv8.{so,dylib} shared library.
+# The latter will contain the precompiled V8 static library from Charles Lowell's libv8 Rubygem.
+# Header files are included from the V8 gem and the Java JDK home directory.
 #
 # Linux and MacOS (darwin) only, no Windows support (yet).
 
@@ -16,16 +15,15 @@ set -e -x
 
 V8_VERSION=7.3.492.27.1
 # Install libv8 Ruby gem
-# sudo gem install libv8 --version $V8_VERSION
+sudo gem install libv8 --version $V8_VERSION
 
-# this also generates/updates JNI header files in /jni
+# this also generates/updates JNI header files in src/main/cpp/
 gradle assemble
 
-# This should work the same in both Linux and Mac
 JAVA_HOME="$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' | grep -oE '\S+$')"
 V8_BASE="$(cd $(dirname $(gem which libv8))/../vendor/v8 ; pwd)"
-INCLUDE_V8="$V8_BASE/include"
-LIBS_V8="$V8_BASE/out.gn/libv8/obj"
+V8_INCLUDE="$V8_BASE/include"
+V8_OBJ="$V8_BASE/out.gn/libv8/obj"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	JAVA_INCLUDES="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
@@ -33,8 +31,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	JAVA_INCLUDES="-I$JAVA_HOME/../include -I$JAVA_HOME/../include/darwin"
 fi
 
-g++ -shared -I$INCLUDE_V8 $JAVA_INCLUDES \
+g++ -shared -I$V8_INCLUDE $JAVA_INCLUDES \
 	src/main/cpp/mv8.cpp \
 	-o libmv8.so \
-	-Wl,$LIBS_V8/libv8_monolith.a \
+	-Wl,$V8_OBJ/libv8_monolith.a \
 	-ldl -pthread -std=c++11 -fPIC

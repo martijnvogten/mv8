169b7961b0886c743da59f6a35c920c9da57d87b
Martijn Vogten <martijn@vogten.nl>
2020-02-11 21:46:42 +0100

    update hello-world.cc and add reacttest.cc

--- samples/hello-world.cc
+++ samples/hello-world.cc
@@ -6,94 +6,98 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "include/libplatform/libplatform.h"
-#include "include/v8.h"
+#include "libplatform/libplatform.h"
+#include "v8.h"
 
-using namespace v8;
-
-void appendFileToString(std::string * str, const char * fileName) {
-    std::string line;
-    std::ifstream in(fileName);
-    while (std::getline(in, line))
-    {
-      *str += line + "\n";
-    }
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   // Initialize V8.
-  // V8::InitializeICUDefaultLocation(argv[0]);
-  V8::InitializeExternalStartupData(argv[0]);
-
-  Platform *platform = platform::CreateDefaultPlatform();
-  V8::InitializePlatform(platform);
-  V8::Initialize();
-
-  std::string text;
-  appendFileToString(&text, "test/jettyv8/react.min.js");
-  appendFileToString(&text, "test/jettyv8/react-dom.min.js");
-  appendFileToString(&text, "test/jettyv8/react-dom-server.min.js");
-  appendFileToString(&text, "test/jettyv8/testlib.js");
-  const char *data = text.c_str();
-
-  v8::StartupData data1 = v8::V8::CreateSnapshotDataBlob(data);
-  v8::StartupData data2 = v8::V8::WarmUpSnapshotDataBlob(data1, "warmup();\n");
-
-  printf("After warming up..\n");
-  fflush(stdout);
+  v8::V8::InitializeICUDefaultLocation(argv[0]);
+  v8::V8::InitializeExternalStartupData(argv[0]);
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  v8::V8::InitializePlatform(platform.get());
+  v8::V8::Initialize();
 
   // Create a new Isolate and make it the current one.
-  Isolate::CreateParams create_params;
-  create_params.snapshot_blob = &data1;
-  ResourceConstraints constraints;
-  constraints.ConfigureDefaults(512000000LL, 512000000LL);
-  constraints.set_max_old_space_size(10);
-  create_params.constraints = constraints;
-
-  create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-
-  Isolate *isolate = Isolate::New(create_params);
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
   {
-    Isolate::Scope isolate_scope(isolate);
+    v8::Isolate::Scope isolate_scope(isolate);
 
     // Create a stack-allocated handle scope.
-    HandleScope handle_scope(isolate);
+    v8::HandleScope handle_scope(isolate);
 
-    Local<Value> resultA;
-    Local<Value> resultB;
+    // Create a new context.
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
 
-    Local<String> source = String::NewFromUtf8(isolate, "doit();", NewStringType::kNormal).ToLocalChecked();
-    printf("After source..\n");fflush(stdout);
 
-    for(int i = 0; i < 100; i++) 
+    // Enter the context for compiling and running the hello world script.
+    v8::Context::Scope context_scope(context);
+
     {
-      Local<Context> context = Context::New(isolate);
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked();
 
-      Context::Scope context_scope(context);
-      Local<Script> script = Script::Compile(context, source).ToLocalChecked();
-      resultA = script->Run(context).ToLocalChecked();
-      String::Utf8Value utf8(resultA);
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+      // Convert the result to an UTF8 string and print it.
+      v8::String::Utf8Value utf8(isolate, result);
       printf("%s\n", *utf8);
     }
 
-    // {
-    //   Local<Context> context = Context::New(isolate);
-    //   Context::Scope context_scope(context);
-    //   Local<Script> script = Script::Compile(context, source).ToLocalChecked();
-    //   resultB = script->Run(context).ToLocalChecked();
-    // }
+    {
+      // Use the JavaScript API to generate a WebAssembly module.
+      //
+      // |bytes| contains the binary format for the following module:
+      //
+      //     (func (export "add") (param i32 i32) (result i32)
+      //       get_local 0
+      //       get_local 1
+      //       i32.add)
+      //
+      const char* csource = R"(
+        let bytes = new Uint8Array([
+          0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
+          0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
+          0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01,
+          0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b
+        ]);
+        let module = new WebAssembly.Module(bytes);
+        let instance = new WebAssembly.Instance(module);
+        instance.exports.add(3, 4);
+      )";
 
-    // // Convert the result to an UTF8 string and print it.
-    // String::Utf8Value utf8B(resultB);
-    // printf("%s\n", *utf8B);
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
+              .ToLocalChecked();
+
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+      // Convert the result to a uint32 and print it.
+      uint32_t number = result->Uint32Value(context).ToChecked();
+      printf("3 + 4 = %u\n", number);
+    }
   }
 
   // Dispose the isolate and tear down V8.
   isolate->Dispose();
-  V8::Dispose();
-  V8::ShutdownPlatform();
-  delete platform;
+  v8::V8::Dispose();
+  v8::V8::ShutdownPlatform();
   delete create_params.array_buffer_allocator;
   return 0;
 }
